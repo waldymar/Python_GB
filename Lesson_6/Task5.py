1699bf7b971c9bf8859347c70e75eabaf2f8873f
waldymar <waldymar@hotmail.com>
2021-03-09 18:34:58 +0200

    5е

--- Lesson_6/Task5.py
+++ Lesson_6/Task5.py
@@ -1,1 +1,26 @@
-print("Все будет хорошо")
+class Stationery:
+
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки! {self.title}")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки ручкой! {self.title}")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки карандашом! {self.title}")
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки маркером! {self.title}")
+
+
+st = Stationery("Привет")
+st.draw()
